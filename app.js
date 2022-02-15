5817cd0c9ebd23302f5651dbdad12483dc134c2d
sebamaza <mazasebastian@hotmail.com>
2022-02-15 17:52:17 -0300

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 8080;
+
+const server = app.listen(8080,()=>(`Listening on ${PORT}`));
+
+app.set('views', './views')
+app.set('view engine', 'ejs')
+app.use(express.json())
+app.use(express.urlencoded({extended:true}))
+
+let products = [];
+
+app.get('/',(req,res)=>{
+    res.render('home',{
+        products
+    })
+})
+
+app.post('/products', (req,res)=>{
+    products.push(req.body);
+    res.redirect('/')
+})
+
